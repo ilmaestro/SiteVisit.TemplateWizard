6c8c57ede15f416421b768421a31a3ba7c9d570a
Ryan.Kilkenny <Ryan.Kilkenny@356f68b5-b2e2-9c48-bf28-bec4e61c41aa>
2012-07-31 19:54:30 +0000

    refactor

---
+++ T4/Partials/SiteVisitFormTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnSite.TemplateWizard.Models;
+using System.IO;
+
+namespace OnSite.TemplateWizard.Classes
+{
+    public class SiteVisitFormTemplate
+    {
+        public enum FileType
+        {
+            Page,
+            Code,
+            Designer
+        }
+        public string OutputFilePath;
+        public SiteVisitForm Form { get; set; }
+        public FileType OutputType { get; set; }
+
+        public void SaveOutput(string output)
+        {
+            File.WriteAllText(OutputFilePath, output); 
+        }
+    }
+}
